677c33bb8e5398de9079e5455c85cfb9957baeeb
t6nn <dev@t6nn.eu>
2017-08-30 13:18:48 +0300

    Jenkins job stub

--- server/deploy_stack.sh
+++ server/deploy_stack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #echo "admin" | docker secret create jenkins-user -
-#echo "admin" | docker secret create jenkins-pass -
+#echo "p4ssw0rd" | docker secret create jenkins-pass -
 
 docker stack deploy -c jenkins.yml jenkins
+
+echo "Done. Now install ant (ant-main, latest version) on http://0.0.0.0:8800/configureTools/"
---
+++ server/scripts/build_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#ls /tmp/codecomp-sessions/ | xargs -I{} ant -f scripts/build.xml -Dprojectdir=/tmp/codecomp-sessions/{}/che-workspace compile
+
+if [[ -z "$1" || ! -d $1 ]]
+  then
+    echo "First parameter must be a directory."
+    exit 1
+fi
+
+for dir in `ls -t $1`
+do
+    PROJECTDIR=$1/${dir}
+    echo
+    echo "Will compile: $dir"
+    cat $PROJECTDIR/player.json
+    echo
+    ant -f build.xml -Dprojectdir=$PROJECTDIR/che-workspace
+done
